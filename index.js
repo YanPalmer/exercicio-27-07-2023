fca93ca1a69676af4db36135c0dae8f5a9dbc243
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-07-27 21:11:32 -0300

    Exercício para resolver

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// Exercício: Identificar a venda de maior valor dentro de todas as vendas de todas as empresas.
+
 import { gerarEmpresas } from "./gerar-empresas.mjs";
 
-const empresas = gerarEmpresas(10);
+const empresas = gerarEmpresas(1);
 
-console.log(empresas);
+console.dir(empresas, {depth: null});
